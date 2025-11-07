b8e26cb6cfde2898ddfea9762a6d98543a9f7dbc
broBbang2 <msk990719@gmail.com>
2025-11-07 22:42:52 +0900

    모범답안 참조 수정

--- src/server.js
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-import app from "./app.js";
-
-const port = Number(process.env.PORT) || 3000;
-
-app.listen(port, () => {
-  console.log(`✅ Server running at http://localhost:${port}`);
-});
